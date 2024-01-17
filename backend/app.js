fc72c214188eeb771b85d8f8ef0c2b8b52d1a191
Toddric <toddric29@gmail.com>
2024-01-17 15:36:31 -0800

    routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('express-async-errors');
+const { ValidationError } = require('sequelize');
 const morgan = require('morgan');
 const cors = require('cors');
 const csurf = require('csurf');
@@ -35,7 +36,35 @@ if (!isProduction) {
     })
   );
   const routes = require('./routes');
-
-
   app.use(routes);
+
+  app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err);
+  });
+  app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      let errors = {};
+      for (let error of err.errors) {
+        errors[error.path] = error.message;
+      }
+      err.title = 'Validation error';
+      err.errors = errors;
+    }
+    next(err);
+  });
+  app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
   module.exports = app;
