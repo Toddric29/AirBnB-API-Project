8715d65fecd9608c5f49ebb18ad1abb4e42105ad
Toddric <toddric29@gmail.com>
2024-01-23 21:59:52 -0800

    all tables created

--- backend/db/seeders/20240124045823-demo-spots.js
+++ backend/db/seeders/20240124045823-demo-spots.js
@@ -1,25 +1,57 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
+const { Spot } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+const spots = [
+  {
+    address: '9786 Twinlake Ave',
+    city: 'Utopia',
+    state: 'Kansas',
+    country: 'USA',
+    lat: 111,
+    lng: -23,
+    name: 'Maj',
+    description: 'A place to jam',
+    price: 123
+  },
+  {
+    address: '3256 Auckland Blvd',
+    city: 'Memphis',
+    state: 'TN',
+    country: 'USA',
+    lat: 324,
+    lng: -20,
+    name: 'Emoh',
+    description: 'A place you can call home',
+    price: 323
+  },
+  {
+    address: '7845 Red St',
+    city: 'Compton',
+    state: 'CA',
+    country: 'USA',
+    lat: 65,
+    lng: -3,
+    name: 'Bunker',
+    description: 'Somewhere you"ll feel safe',
+    price: 400
+  }
+]
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await Spot.bulkCreate(spots, { validate: true });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Spots', {
+      address: spots.map(spot => spot.address)
+    }, {});
   }
 };
